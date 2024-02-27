7259fa1597d6ed35071dda9a1238aabf27655911
Vince489 <vincentcornelius489@gmail.com>
2024-02-27 15:36:25 -0500

    cors

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 const app = express();
 
 const corsOptions = {
-  origin: ["http://localhost:3000", "https://zprompter.com", "https://virtronboxing.club", "https://virtron-beta.netlify.app/"],
+  origin: ["http://localhost:3000", "https://zprompter.com", "https://virtronboxing.club", "https://virtron-beta.netlify.app", "https://virtron-beta.netlify.app/"],
   credentials: true,
 };
 
